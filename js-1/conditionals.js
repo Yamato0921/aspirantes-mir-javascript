d9618e12b97b01ddb0b4841372fd83ecf9d8ebb6
Yamato0921 <sebastiangiraldocorpas@gmail.com>
2023-05-17 17:22:37 -0500

    soucion codigo

--- js-1/conditionals.js
+++ js-1/conditionals.js
@@ -2,12 +2,12 @@ function calcularColor(num) {
     var color;
     if (num == 1) {
       color = "black";
-    } else if (num = 2) {
+    } else if (num == 2) {
       color = "blanco";
     } else if (num == 3) {
       color = "rojo";
     } else {
-      color: "verde";
+      color = "verde";
     }
   
     return "El color es " + color;
